69b1c33d14addf307fa929772996a79d3c7126fa
Jon Maciel <joaomaciel.n@gmail.com>
2017-07-17 10:25:59 -0300

    gambi

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import logo from './logo.svg'
 import {Editor, EditorState, getDefaultKeyBinding, KeyBindingUtil, CompositeDecorator, SelectionState, Modifier} from 'draft-js'
 import './App.css'
 const {hasCommandModifier} = KeyBindingUtil
-const HANDLE_REGEX = /\%\{[\w\?]+/g
+const HANDLE_REGEX = /\%\{[^{]+\}/g
+const INVALID_REGEX = /\%\{[\w]+ /g
 const styles = {
   handle: {
     color: 'rgba(98, 177, 254, 1.0)',
@@ -13,161 +14,159 @@ const styles = {
   }
 }
 
-const HandleSpan = (props) => (
-  <span style={styles.handle} data-offset-key={props.offsetKey} contentEditable={false}>
-    <a> Title X </a>
-    <span style={{display: 'none'}}>
-      {props.children}
-    </span>
-  </span>
-)
-
 class App extends Component {
   constructor(props) {
     super(props)
     this.myKeyBindingFn = this.myKeyBindingFn.bind(this)
     this.handleStrategy = this.handleStrategy.bind(this)
+    this.handleSpan = this.handleSpan.bind(this)
     this.onChange = this.onChange.bind(this)
+    this.addVar = this.addVar.bind(this)
 
-    const compositeDecorator = new CompositeDecorator([{strategy: this.handleStrategy, component: HandleSpan}])
-    this.state = {editorState: EditorState.createEmpty(compositeDecorator)}
-  }
-
-  onChange(editorState) {
-    // let currentContent = editorState.getCurrentContent()
-    // const selection = editorState.getSelection();
-    // let blockMap = currentContent.getBlockMap()
-
-    // const block = editorState.getCurrentContent().getBlockForKey(selection.getAnchorKey());
-    // var selectionState = editorState.getSelection()
-    // if (block.getText().indexOf("--") !== -1) {
-    //       // Replace '--' with '*'
-    //   const contentWithoutDash = Modifier.replaceText(
-    //       editorState.getCurrentContent(),
-    //       new SelectionState({
-    //         anchorKey: block.getKey(),
-    //         anchorOffset: 0,
-    //         focusKey: block.getKey(),
-    //         focusOffset: 2
-    //       }),
-    //       "*");
-    //   editorState = EditorState.push(
-    //       editorState,
-    //       contentWithoutDash,
-    //       'replace-text'
-    //   );
-    // }
-    this.setState({editorState});
-
-
-
-/*
-    var newBlockMap = blockMap.map((contentBlock) => contentBlock.set('text', contentBlock.getText().replace('aa', '')))
-
-    currentContent = currentContent.set('blockMap', newBlockMap)
-
-    this.setState({
-      editorState: EditorState.push(editorState, currentContent, 'remove-text')
-    })
-*/
-    // this.setState({editorState})
-  }
-
- handleBeforeInput(e) {
-    if(e === ' ') {
-      const { editorState } = this.state;
-      const content = editorState.getCurrentContent();
-      const selection = editorState.getSelection();
-      const end = selection.getEndOffset();
-
-      const block = content.getBlockForKey(selection.getAnchorKey());
-      const word = block.getText();
-      const result = word.slice(this.state.lastOffset, selection.getEndOffset());
-      const newSelection = new SelectionState({
-          anchorKey: block.getKey(),
-          anchorOffset: 0,
-          focusKey: block.getKey(),
-          focusOffset: 0
-      })
-      const contentReplaced = Modifier.removeRange(
-                content,
-                newSelection)
-      const editorStateModified = EditorState.push(
-         editorState,
-         contentReplaced,
-         'replace-text'
-      );
-
-      this.setState({lastOffset: selection.getEndOffset(), editorState:  editorStateModified})
-      return true;
-    } else {
-      return false;
+    const compositeDecorator = new CompositeDecorator([{strategy: this.handleStrategy, component: this.handleSpan}])
+    this.state = {
+      editorState: EditorState.createEmpty(compositeDecorator),
+      varOfSet: [],
     }
   }
 
-  myKeyBindingFn(e: SyntheticKeyboardEvent): string {
-    console.log(e.keyCode)
-    if ([13, 219, 221].includes(e.keyCode)) { return false }
+  handleSpan(props) {
+    const componentStartIndex = props.contentState.selectionBefore.anchorOffset
+    const componentEndIndex = props.contentState.selectionAfter.anchorOffset
+
+    return (
+      <span style={styles.handle} data-offset-key={props.offsetKey} contentEditable={false}>
+        <a>
+          Field
+          <span onClick={() => this.removeVar(componentStartIndex, componentEndIndex)}>X</span>
+        </a>
+        <span style={{display: 'none'}}>{props.children}</span>
+      </span>
+    )
+  }
+
+  onChange(editorState) { this.setState({editorState}) }
+
+  myKeyBindingFn(e): string {
+    // if(e.keyCode === 13) { return false }
+    if(e.keyCode == 8) {
+      var editorState = this.state.editorState
+      var selectionState = editorState.getSelection()
+      var start = selectionState.getStartOffset()
+      var end = selectionState.getEndOffset()
+      let varIndex = this.findVarOfset(start - 1)
+
+      if(varIndex < 0 && start != end) {
+        varIndex = this.findVarOfset(end)
+      }
+
+      if(varIndex >= 0) {
+        this.removeVar(this.state.varOfSet[varIndex].start, this.state.varOfSet[varIndex].end + 1)
+      }
+    }
+
+    if(e.keyCode == 46) {
+
+    }
+    return false
   }
 
   handleStrategy(contentBlock, callback, contentState) {
-    this.findWithRegex(HANDLE_REGEX, contentBlock, callback, contentState)
+    this.findWithRegex(HANDLE_REGEX, contentBlock, callback)
   }
 
-
-  findWithRegex(regex, contentBlock, callback, contentState) {
+  findWithRegex(regex, contentBlock, callback) {
     const text = contentBlock.getText()
-    let matchArr, start, newText, newContentState, newBlockMap
-    const blockMap = contentState.getBlockMap()
-
+    let varOfSet = []
+    let matchArr, start, end
 
     while ((matchArr = regex.exec(text)) !== null) {
       start = matchArr.index
-      newBlockMap = blockMap.map((contentBlock) => {
-        newText = contentBlock.getText().replace(contentBlock.getText().slice(start, start + matchArr[0].length), '')
-        return contentBlock.set('text', newText)
-      })
-
-
-      // newContentState = contentState.set('blockMap', newBlockMap)
-      // const newEditorState = this.state.editorState
-
-      // this.setState({
-      //   editorState: EditorState.push(newEditorState, newContentState, 'change-block-data')
-      // })
-
-      // console.log(contentState.toJSON())
-      // callback(start, start + matchArr[0].length)
+      end = start + matchArr[0].length
+      varOfSet.push({start, end})
+      callback(start, end)
     }
+    this.setState({varOfSet})
+  }
 
+  findVarOfset(index) {
+    const varOfSet = this.state.varOfSet
+    for (let i = 0; i < varOfSet.length; i++) {
+      if(varOfSet[i].start <= index && index <= varOfSet[i].end) { return i }
+    }
+    return -1
+  }
+
+  addVar() {
+    let editorState = this.state.editorState
+    let selectionState = editorState.getSelection()
+
+    this.setState({
+      editorState: EditorState.push(
+        editorState,
+        Modifier.replaceText(
+          editorState.getCurrentContent(),
+          selectionState,
+          '%{title}'
+        ),
+        'replace-text'
+      )
+    })
+  }
+
+  removeVar(start, end) {
+    let editorState = this.state.editorState
+    let varOfSet = [...this.state.varOfSet]
+    let selectionState = editorState.getSelection()
+
+    selectionState = selectionState.merge({anchorOffset: start, focusOffset: end})
+    varOfSet.splice(this.findVarOfset(start), 1)
+
+    this.setState({
+      varOfSet,
+      editorState: EditorState.push(
+        editorState,
+        Modifier.removeRange(
+          editorState.getCurrentContent(),
+          selectionState,
+          'forward'
+        ),
+        'remove-text'
+      )
+    })
   }
 
   render() {
     var editorState = this.state.editorState
     var selectionState = editorState.getSelection()
-    var anchorKey = selectionState.getAnchorKey()
-    var currentContent = editorState.getCurrentContent()
-    var currentContentBlock = currentContent.getBlockForKey(anchorKey)
-    var start = selectionState.getStartOffset()
-    var end = selectionState.getEndOffset()
-    var selectedText = currentContentBlock.getText().slice(start, end)
-
+    console.log(selectionState.getEndOffset())
+    console.log(this.state.varOfSet)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
+        <div className="App-header"><img src={logo} className="App-logo" alt="logo" /></div>
         <br />
         <div style={{'background-color': '#00ff00'}}>
           <Editor editorState={this.state.editorState} onChange={this.onChange} keyBindingFn={this.myKeyBindingFn} />
         </div>
         <br/>
-        <div>
-          {this.state.editorState.getCurrentContent().getPlainText()}
-        </div>
+        <div>{this.state.editorState.getCurrentContent().getPlainText()}</div>
+        <br/>
+        <div><a href="#" onClick={this.addVar}>ADD VAR</a></div>
       </div>
     )
   }
 }
 
 export default App
+
+
+/*
+  var editorState = this.state.editorState
+  var selectionState = editorState.getSelection()
+  var anchorKey = selectionState.getAnchorKey()
+  var currentContent = editorState.getCurrentContent()
+  var currentContentBlock = currentContent.getBlockForKey(anchorKey)
+  var start = selectionState.getStartOffset()
+  var end = selectionState.getEndOffset()
+  var selectedText = currentContentBlock.getText().slice(start, end)
+*/
