acdd5b6a0dad333d488fdce02dd55f8017ce773b
Jon Maciel <joaomaciel.n@gmail.com>
2017-07-17 18:02:25 -0300

    better waaaaaaaaaay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // @flow
 import React, { Component } from 'react'
+import { List } from 'immutable'
 import logo from './logo.svg'
-import {Editor, EditorState, getDefaultKeyBinding, KeyBindingUtil, CompositeDecorator, SelectionState, Modifier} from 'draft-js'
+import {Editor, EditorState, KeyBindingUtil, CompositeDecorator, ContentState, Entity, SelectionState, Modifier, ContentBlock, genKey} from 'draft-js'
 import './App.css'
 const {hasCommandModifier} = KeyBindingUtil
 const HANDLE_REGEX = /\%\{[^{]+\}/g
@@ -17,17 +18,13 @@ const styles = {
 class App extends Component {
   constructor(props) {
     super(props)
-    this.myKeyBindingFn = this.myKeyBindingFn.bind(this)
     this.handleStrategy = this.handleStrategy.bind(this)
     this.handleSpan = this.handleSpan.bind(this)
     this.onChange = this.onChange.bind(this)
     this.addVar = this.addVar.bind(this)
 
     const compositeDecorator = new CompositeDecorator([{strategy: this.handleStrategy, component: this.handleSpan}])
-    this.state = {
-      editorState: EditorState.createEmpty(compositeDecorator),
-      varOfSet: [],
-    }
+    this.state = {editorState: EditorState.createEmpty(compositeDecorator)}
   }
 
   handleSpan(props) {
@@ -47,30 +44,6 @@ class App extends Component {
 
   onChange(editorState) { this.setState({editorState}) }
 
-  myKeyBindingFn(e): string {
-    // if(e.keyCode === 13) { return false }
-    if(e.keyCode == 8) {
-      var editorState = this.state.editorState
-      var selectionState = editorState.getSelection()
-      var start = selectionState.getStartOffset()
-      var end = selectionState.getEndOffset()
-      let varIndex = this.findVarOfset(start - 1)
-
-      if(varIndex < 0 && start != end) {
-        varIndex = this.findVarOfset(end)
-      }
-
-      if(varIndex >= 0) {
-        this.removeVar(this.state.varOfSet[varIndex].start, this.state.varOfSet[varIndex].end + 1)
-      }
-    }
-
-    if(e.keyCode == 46) {
-
-    }
-    return false
-  }
-
   handleStrategy(contentBlock, callback, contentState) {
     this.findWithRegex(HANDLE_REGEX, contentBlock, callback)
   }
@@ -80,50 +53,42 @@ class App extends Component {
     let varOfSet = []
     let matchArr, start, end
 
-    while ((matchArr = regex.exec(text)) !== null) {
-      start = matchArr.index
-      end = start + matchArr[0].length
-      varOfSet.push({start, end})
-      callback(start, end)
-    }
-    this.setState({varOfSet})
-  }
 
-  findVarOfset(index) {
-    const varOfSet = this.state.varOfSet
-    for (let i = 0; i < varOfSet.length; i++) {
-      if(varOfSet[i].start <= index && index <= varOfSet[i].end) { return i }
-    }
-    return -1
+    contentBlock.findEntityRanges((character) => {
+      var entityKey = character.getEntity();
+      return entityKey !== null && Entity.get(entityKey).getType() === 'FIELDVAR';
+    }, callback)
   }
 
   addVar() {
-    let editorState = this.state.editorState
-    let selectionState = editorState.getSelection()
+    var label = '%{teste vizu}'
+    const editorState = this.state.editorState;
+    const currentContent = editorState.getCurrentContent();
+    const selection = editorState.getSelection();
+    const entityKey = Entity.create('FIELDVAR', 'IMMUTABLE', {});
 
     this.setState({
-      editorState: EditorState.push(
-        editorState,
-        Modifier.replaceText(
-          editorState.getCurrentContent(),
-          selectionState,
-          '%{title}'
-        ),
-        'replace-text'
-      )
-    })
+        editorState: EditorState.push(
+          editorState,
+          Modifier.replaceText(currentContent, selection, label, null, entityKey),
+          'replace-text'
+        )
+    });
+
   }
 
   removeVar(start, end) {
+    // var currentContentState = currentEditorState.getCurrentContent();
+
+    // var newContentState = Modifier.applyEntity(currentContentState, currentEditorState.getSelection(), null);
+    // var newEditorState = EditorState.push(currentEditorState, newContentState, 'apply-entity')
+
     let editorState = this.state.editorState
-    let varOfSet = [...this.state.varOfSet]
     let selectionState = editorState.getSelection()
 
     selectionState = selectionState.merge({anchorOffset: start, focusOffset: end})
-    varOfSet.splice(this.findVarOfset(start), 1)
 
     this.setState({
-      varOfSet,
       editorState: EditorState.push(
         editorState,
         Modifier.removeRange(
@@ -139,14 +104,12 @@ class App extends Component {
   render() {
     var editorState = this.state.editorState
     var selectionState = editorState.getSelection()
-    console.log(selectionState.getEndOffset())
-    console.log(this.state.varOfSet)
     return (
       <div className="App">
         <div className="App-header"><img src={logo} className="App-logo" alt="logo" /></div>
         <br />
         <div style={{'background-color': '#00ff00'}}>
-          <Editor editorState={this.state.editorState} onChange={this.onChange} keyBindingFn={this.myKeyBindingFn} />
+          <Editor editorState={this.state.editorState} onChange={this.onChange}/>
         </div>
         <br/>
         <div>{this.state.editorState.getCurrentContent().getPlainText()}</div>
