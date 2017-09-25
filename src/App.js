1bde04c1753d5a813e81b17bb4ce6dbee91ff2df
Jon Maciel <joaomaciel.n@gmail.com>
2017-09-25 18:13:58 -0300

    step5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 // @flow
 import React, { Component } from 'react'
-import { List } from 'immutable'
 import logo from './logo.svg'
-import {Editor, EditorState, KeyBindingUtil, CompositeDecorator, ContentState, Entity, SelectionState, Modifier, ContentBlock, genKey} from 'draft-js'
+import { Editor, EditorState, CompositeDecorator, Entity, Modifier, ContentState } from 'draft-js'
 import './App.css'
-const {hasCommandModifier} = KeyBindingUtil
-const HANDLE_REGEX = /\%\{[^{]+\}/g
-const INVALID_REGEX = /\%\{[\w]+ /g
+
 const styles = {
   handle: {
     color: 'rgba(98, 177, 254, 1.0)',
@@ -18,81 +15,69 @@ const styles = {
 class App extends Component {
   constructor(props) {
     super(props)
-    this.handleStrategy = this.handleStrategy.bind(this)
-    this.handleSpan = this.handleSpan.bind(this)
-    this.onChange = this.onChange.bind(this)
-    this.addVar = this.addVar.bind(this)
+    const compositeDecorator = new CompositeDecorator([{ strategy: this.fieldVarStrategy, component: this.decoratorComponent }])
 
-    const compositeDecorator = new CompositeDecorator([{strategy: this.handleStrategy, component: this.handleSpan}])
-    this.state = {editorState: EditorState.createEmpty(compositeDecorator)}
+    this.state = {
+      editorState: EditorState.createWithContent(
+        ContentState.createFromText('Hello World! 🍺🍺🍺'),
+        compositeDecorator
+      )
+    }
   }
 
-  handleSpan(props) {
-    const componentStartIndex = props.contentState.selectionBefore.anchorOffset
-    const componentEndIndex = props.contentState.selectionAfter.anchorOffset
-
+  decoratorComponent = (props) => {
+    // props.children
     return (
       <span style={styles.handle} data-offset-key={props.offsetKey} contentEditable={false}>
-        <a>
-          Field
-          <span onClick={() => this.removeVar(componentStartIndex, componentEndIndex)}>X</span>
-        </a>
-        <span style={{display: 'none'}}>{props.children}</span>
+        <a>Variable <span onClick={() => this.removeVarByEntity(props.entityKey)}>X</span></a>
       </span>
     )
   }
 
-  onChange(editorState) { this.setState({editorState}) }
+  onChange = (editorState) => { this.setState({editorState}) }
 
-  handleStrategy(contentBlock, callback, contentState) {
-    this.findWithRegex(HANDLE_REGEX, contentBlock, callback)
-  }
-
-  findWithRegex(regex, contentBlock, callback) {
-    const text = contentBlock.getText()
-    let varOfSet = []
-    let matchArr, start, end
-
-
+  fieldVarStrategy = (contentBlock, callback, contentState) => {
     contentBlock.findEntityRanges((character) => {
-      var entityKey = character.getEntity();
-      return entityKey !== null && Entity.get(entityKey).getType() === 'FIELDVAR';
+      let entityKey = character.getEntity();
+      return entityKey !== null && contentState.getEntity(entityKey).getType() === 'FIELDVAR';
     }, callback)
   }
 
-  addVar() {
-    var label = '%{teste vizu}'
+  addVar = () => {
+    let label = '%{VALUE}'
     const editorState = this.state.editorState;
-    const currentContent = editorState.getCurrentContent();
+    const contentState = editorState.getCurrentContent();
     const selection = editorState.getSelection();
     const entityKey = Entity.create('FIELDVAR', 'IMMUTABLE', {});
 
     this.setState({
         editorState: EditorState.push(
           editorState,
-          Modifier.replaceText(currentContent, selection, label, null, entityKey),
+          Modifier.replaceText(contentState, selection, label, null, entityKey),
           'replace-text'
         )
     });
-
   }
 
-  removeVar(start, end) {
-    // var currentContentState = currentEditorState.getCurrentContent();
-
-    // var newContentState = Modifier.applyEntity(currentContentState, currentEditorState.getSelection(), null);
-    // var newEditorState = EditorState.push(currentEditorState, newContentState, 'apply-entity')
-
+  removeVarByEntity = (entityKey) => {
     let editorState = this.state.editorState
-    let selectionState = editorState.getSelection()
+    let contentState = editorState.getCurrentContent()
 
-    selectionState = selectionState.merge({anchorOffset: start, focusOffset: end})
+    contentState.getBlockMap().forEach((block) => {
+      block.findEntityRanges((character) => (entityKey === character.getEntity()), this.removeVar)
+    })
+  }
+
+  removeVar = (start, end) => {
+    let editorState = this.state.editorState
+    let contentState = editorState.getCurrentContent()
+    let selectionState = editorState.getSelection().merge({anchorOffset: start, focusOffset: end})
 
     this.setState({
       editorState: EditorState.push(
         editorState,
         Modifier.removeRange(
-          editorState.getCurrentContent(),
+          contentState,
           selectionState,
           'forward'
         ),
@@ -102,34 +87,22 @@ class App extends Component {
   }
 
   render() {
-    var editorState = this.state.editorState
-    var selectionState = editorState.getSelection()
     return (
       <div className="App">
         <div className="App-header"><img src={logo} className="App-logo" alt="logo" /></div>
         <br />
-        <div style={{'background-color': '#00ff00'}}>
+        <div style={{'backgroundColor': '#00ff00'}}>
           <Editor editorState={this.state.editorState} onChange={this.onChange}/>
         </div>
         <br/>
         <div>{this.state.editorState.getCurrentContent().getPlainText()}</div>
         <br/>
-        <div><a href="#" onClick={this.addVar}>ADD VAR</a></div>
+        <div>
+          <a href="#AddVar" onClick={this.addVar}>ADD VAR</a>
+        </div>
       </div>
     )
   }
 }
 
 export default App
-
-
-/*
-  var editorState = this.state.editorState
-  var selectionState = editorState.getSelection()
-  var anchorKey = selectionState.getAnchorKey()
-  var currentContent = editorState.getCurrentContent()
-  var currentContentBlock = currentContent.getBlockForKey(anchorKey)
-  var start = selectionState.getStartOffset()
-  var end = selectionState.getEndOffset()
-  var selectedText = currentContentBlock.getText().slice(start, end)
-*/
