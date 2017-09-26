92ee5fdd26d5eb5b3c5babcad73e7c83abfc13e3
Jon Maciel <joaomaciel.n@gmail.com>
2017-09-26 00:06:27 -0300

    lol

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     }
   }
 
-  onChange = (editorState) => { this.setState({editorState}) }
+  onChange = (editorState) => this.setState({editorState})
 
   render() {
     return (
